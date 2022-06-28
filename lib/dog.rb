dc627232da40175f9049518fcf6335da82088637
Daniel Yang <danyang0724@gmail.com>
2022-06-28 09:09:56 -0400

    commit tree c07df3e4c2d29d3c99057ccb5a1a1627cb8bd937

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+  fido = Dog.new
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+  ada = Person.new
+
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+  
+end
